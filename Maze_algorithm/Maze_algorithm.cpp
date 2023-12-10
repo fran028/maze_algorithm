8e94b347c63c3be384a117a74628d1e9211c6a0b
fran028 <60265684+fran028@users.noreply.github.com>
2023-12-10 19:16:19 +0100

    Subida inicio proyecto

---
+++ Maze_algorithm/Maze_algorithm.cpp
@@ -0,0 +1,12 @@
+﻿// Maze_algorithm.cpp : Defines the entry point for the application.
+//
+
+#include "Maze_algorithm.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
