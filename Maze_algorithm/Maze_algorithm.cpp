953e1ec59f7f0c3874b76a25f105e2e43abd1578
fran028 <60265684+fran028@users.noreply.github.com>
2023-12-19 18:39:41 +0100

    SolveMazeLIFO function finished
    The function that solved the maze recursively with the auxiliar function that check for the next

--- Maze_algorithm/Maze_algorithm.cpp
+++ Maze_algorithm/Maze_algorithm.cpp
@@ -4,33 +4,33 @@
 #include "Maze_algorithm.h"
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
 using namespace std;
+
+
 
 
 class MazeCell {
 	public:
-		char state;
-		bool tempMark;
-		int x;
-		int y;
+		char state; //w,f,s,c
+		bool tempMark;//treu o false
+		int x; //int
+		int y; //int
 };
 
 class Position {
 	public: 
 		int x;
 		int y;
+
+		Position(int setx, int sety) {  
+			x = setx;
+			y = sety;
+		}
 };
 
-
-//MazeCell*  mazeMatrix[10][10];
-
-/*MazeCell** mazeMatrix = nullptr;  // Pointer to the array of pointers
-int rows = 5, columns = 10;
-
-mazeMatrix = new MazeCell * [rows];  // Allocate memory for the array of pointers
-for (int i = 0; i < rows; i++) {
-	mazeMatrix[i] = new MazeCell[columns];  // Allocate memory for each row
-}*/
+vector<vector<MazeCell>> Maze;
 
 
 void Menu() {
@@ -62,73 +62,101 @@ void optionManager( int op) {
 	} 
 }
 
-void LoadMazeMap() {
+int LoadMazeMap() {
 	string filename;
-	cout << "Enter file name where maze is located: ";
-	cin >> filename; 
+	cout << "Enter file name where maze is located with file type: ";
+	cin >> filename;  
+	int col, row;
+
+	cout << "Enter number of columns of maze: ";
+	cin >> col;
+	cout << "Enter number of rows of maze: ";
+	cin >> row;
+	Maze.reserve(row);
+	for (int i = 0; i < Maze.size(); ++i) {
+		Maze[i].reserve(col);
+		//use assign to fill all the maze with default wall
+	}
+
+	fstream leer(filename, ios::in);
+    if (leer.fail()) {
+        cout << "Maze file does not exist";
+        return 0;
+    }
+    string linea;  
+    while (getline(leer, linea)) {
+		istringstream iss(linea);
+		string value;
+		vector<std::string> auxValues;
+		while (getline(iss, value, ',')) {
+			auxValues.push_back(value);
+		}
+		//check if type is not wall and only add the 'c','f' and 's' types
+		//Also check if a 's' and 'f' types exist in file (f>=1 and s>=1)
 
 
+    }
+    leer.close();  
+	return 1;
 }
 
 Position NextMove(MazeCell* cell, MazeCell* lastCell) {
 	int dirH = cell->x - lastCell->x;
 	int dirV = cell->y - lastCell->y;
+	int x1 = -1;
+	int y1 = -1;
 
 	//derecha
 	if (dirH == 0) {
-		int x1 = cell->x - dirV;
-		int y1 = cell->y;
-	}
-	else {
-		int x1 = cell->x;
-		int y1 = cell->y - dirH;
+		x1 = cell->x - dirV;
+		y1 = cell->y;
+	} else {
+		x1 = cell->x;
+		y1 = cell->y - dirH;
 	}  
-
-	if (cell[x1][y1].state == 'c' && not cell[x1][y1].tempMark) {
-		return (x1,y1)
+	if (Maze[x1][y1].state == 'c' && not Maze[x1][y1].tempMark) {
+		Position nextPos(x1,y1);
+		return nextPos;
 	}
 
 	//adelante 
-	int x1 = cell->x + dirV;
-	int y1 = cell->y + dirH; 
-
-	if (cell[x1][y1].state == 'c' && not cell[x1][y1].tempMark) {
-		return (x1, y1)
+	x1 = cell->x + dirV;
+	y1 = cell->y + dirH; 
+	if (Maze[x1][y1].state == 'c' && not Maze[x1][y1].tempMark) {
+		Position nextPos(x1, y1);
+		return nextPos; 
 	}
 
 	//izquierda
 	if (dirH == 0) {
-		int x1 = cell->x + dirV;
-		int y1 = cell->y;
+		x1 = cell->x + dirV;
+		y1 = cell->y;
 	}
 	else {
-		int x1 = cell->x;
-		int y1 = cell->y + dirH;
+		x1 = cell->x;
+		y1 = cell->y + dirH;
+	}
+	if (Maze[x1][y1].state == 'c' && not Maze[x1][y1].tempMark) {
+		Position nextPos(x1, y1);
+		return nextPos;
 	}
 
-	if (cell[x1][y1].state == 'c' && not cell[x1][y1].tempMark) {
-		return (x1, y1)
-	}
-
-	return (-1, -1);
+	Position nextPos(x1, y1);
+	return nextPos; 
 }
 
 
-bool SolveMazeLIFO(MazeCell *cell, MazeCell *lastCell) {
-	
-	bool salida = false;
-	
+bool SolveMazeLIFO(MazeCell *cell, MazeCell *lastCell) {  
 	if (cell->state == 'f'){
 		return true;
 	}
-
+	bool salida = false;
 	cell->tempMark = true;
 	Position nextPos = NextMove(cell, lastCell);
-	while(nextpos != (-1,-1) and not salida){
-		salida = SolveMazeLIFO(cell[nextPos.x][nextPos.y], cell);
+	while( (nextPos.x != -1 and nextPos.y!=-1) and not salida){
+		salida = SolveMazeLIFO(&Maze[nextPos.x][nextPos.y], cell);
 		Position nextPos = NextMove(cell, lastCell);
-	}
-
+	} 
 	return salida;
 }
 
